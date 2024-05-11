2e72b76533847334aaa1c41225d80ed69db6eebb
J-odion <emmyjaff22@gmail.com>
2024-05-11 17:05:26 +0100

    fix: for pressable to Touchable

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,34 +1,28 @@
-import { StatusBar } from "expo-status-bar";
 import {
   Image,
   ImageBackground,
-  Pressable,
-  StyleSheet,
-  Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { FontAwesome6 } from "@expo/vector-icons";
 
 export default function HomeScreen({ navigation }) {
-  
   const imag = require("../assets/images/bgimage.png");
-  
   const Logo = require("../assets/images/whiteLogo.png");
   return (
     <View style={{ backgroundColor: "#9F1419", flex: 1 }}>
-      
       <ImageBackground source={imag} style={{ flex: 1 }} resizeMode="cover">
         <View style={{ flex: 1, alignItems: "center",justifyContent: "center",}}>
           <Image source={Logo} resizeMode="cover" />
           <View>
-            <Pressable onPress={() => navigation.push("HomeSlider")}>
+            <TouchableOpacity onPress={() => navigation.push("HomeSlider")}>
               <FontAwesome6
                 name="circle-arrow-right"
                 size={50}
                 color="#D9D9D9"
                 style={{top: 150,}}
               />
-            </Pressable>
+            </TouchableOpacity>
           </View>
         </View>
       </ImageBackground>
